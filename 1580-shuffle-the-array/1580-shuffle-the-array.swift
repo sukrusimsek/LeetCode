6982231aba3927854b914abef452d4343100ef12
Şükrü Şimşek <107575752+sukrusimsek@users.noreply.github.com>
2023-10-30 00:44:44 +0300

    Time: 24 ms (94.57%), Space: 14.1 MB (73.64%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+        var result = [Int]()
+        for i in 0..<n {
+            result.append(nums[i])
+            result.append(nums[n + i])
+        }
+        return result
+    }
+}
+        
