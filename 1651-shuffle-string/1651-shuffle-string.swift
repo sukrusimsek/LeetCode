c3e38031c08011dc563cf86c3dd91e0384a0db01
Şükrü Şimşek <107575752+sukrusimsek@users.noreply.github.com>
2023-10-29 15:16:32 +0300

    Time: 18 ms (100%), Space: 14.6 MB (56.16%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.swift
+++ 1651-shuffle-string/1651-shuffle-string.swift
@@ -1,6 +1,6 @@
 class Solution {
     func restoreString(_ s: String, _ indices: [Int]) -> String {
-        var arr : [Character] = Array(repeating: "S",count: indices.count)
+        var arr : [Character] = Array(repeating: "s",count: indices.count)
         let char = Array(s)
         for i in 0..<indices.count {
             arr[indices[i]] = char[i]
