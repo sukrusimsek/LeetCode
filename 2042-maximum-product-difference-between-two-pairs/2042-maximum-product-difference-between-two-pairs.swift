a8eb22c78f1cb57a8bc6e00b6d5be99fd3b7f648
Şükrü Şimşek <107575752+sukrusimsek@users.noreply.github.com>
2023-11-11 14:29:10 +0300

    Time: 158 ms (14.29%), Space: 14.3 MB (68.57%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.swift
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.swift
@@ -1,11 +1,7 @@
 class Solution {
     func maxProductDifference(_ nums: [Int]) -> Int {
         let sortNums = nums.sorted()
-        let highV = sortNums.max()!
-        let highV2 = sortNums[sortNums.count-2]
-        let minV = sortNums[0]
-        let minV2 = sortNums[1]
-        let result = ((highV * highV2) - (minV * minV2))
-        return result
+        let result = ((sortNums[0] * sortNums[1]) - (sortNums[sortNums.count-1] * sortNums[sortNums.count-2]))
+        return -result
     }
 }
