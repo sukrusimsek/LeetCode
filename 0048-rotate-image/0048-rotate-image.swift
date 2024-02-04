aed60eb7681571f17ba211563a7f0cd890221d71
Şükrü Şimşek <107575752+sukrusimsek@users.noreply.github.com>
2024-02-04 19:48:15 +0300

    Time: 9 ms (53.98%), Space: 15.9 MB (6.19%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func rotate(_ matrix: inout [[Int]]) {
+        var counter = 0
+        var colMain = matrix
+        matrix.removeAll()
+        for i in colMain {
+            var index = colMain.map { $0[counter] }
+            counter += 1
+            let reverseIndex = Array(index.reversed())
+            matrix.append(reverseIndex)
+        }
+        print(matrix)
+    }
+}
