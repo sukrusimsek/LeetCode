ee1c5aedc5481c47217b9e46352bbc258e30558b
Şükrü Şimşek <107575752+sukrusimsek@users.noreply.github.com>
2024-02-19 22:43:02 +0300

    Time: 4 ms (59.2%), Space: 15.3 MB (8%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.swift
+++ 0231-power-of-two/0231-power-of-two.swift
@@ -1,6 +1,14 @@
 class Solution {
     func isPowerOfTwo(_ n: Int) -> Bool {
         if n <= 0 { return false }
-        return n & (n - 1) == 0
-    }
+        var number = n
+        while number > 1 {
+            if number % 2 != 0 {
+                return false
+            }
+            number /= 2
+        }
+    
+    return true
+    }   
 }
